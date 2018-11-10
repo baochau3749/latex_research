b2adde06f89195f669607d4b5e3f5fb9700add86
Bao Chau <chaub@oregonstate.edu>
2018-11-10 15:41:04 -0800

    Add static folder and files

--- src/main/java/com/cs493/LatexResearch/Controller/MainController.java
+++ src/main/java/com/cs493/LatexResearch/Controller/MainController.java
@@ -14,7 +14,7 @@ public class MainController {
 	public String main(Model theModel) {
 		System.out.println(">>> Hello.............");
 		
-		return "Hello";
+		return "Hello - Add Static folder";
 	}
 	
 }
