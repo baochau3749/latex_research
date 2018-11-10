99da73b2b55b8eeb0f44b0357705391fe174e66d
Bao Chau <chaub@oregonstate.edu>
2018-11-10 13:32:52 -0800

    Initial project code

---
+++ src/main/java/com/cs493/LatexResearch/Controller/MainController.java
@@ -0,0 +1,20 @@
+package com.cs493.LatexResearch.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String main(Model theModel) {
+		System.out.println(">>> Hello.............");
+		
+		return "Hello";
+	}
+	
+}
---
+++ src/main/java/com/cs493/LatexResearch/LatexResearchApplication.java
@@ -0,0 +1,12 @@
+package com.cs493.LatexResearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatexResearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatexResearchApplication.class, args);
+	}
+}
