bedbf42a4c62458286cf4a9d38f783fa3273eeaf
Bao Chau <chaub@oregonstate.edu>
2018-11-10 15:48:34 -0800

    Updated main controller

--- src/main/java/com/cs493/LatexResearch/Controller/MainController.java
+++ src/main/java/com/cs493/LatexResearch/Controller/MainController.java
@@ -1,5 +1,10 @@
 package com.cs493.LatexResearch.Controller;
 
+
+import java.io.IOException;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +21,25 @@ public class MainController {
 //		
 //		return "Hello - Add latex_files folder";
 //	}
+	
+public static final Resource LATEX_DIR = new ClassPathResource("/latex_files");
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String main(Model theModel) throws IOException {
+		
+		System.out.println(">>> Hello.............");
+				
+		String latexFolder = LATEX_DIR.getFile().getAbsolutePath();
+		String pdfDoc = latexFolder + "\\hello.pdf";
+		
+		System.out.println(">>> latexFolder = " + latexFolder);
+		System.out.println(">>> pdfDoc = " + pdfDoc);
+		
+		theModel.addAttribute("latexFolder", latexFolder);
+		theModel.addAttribute("pdfDoc", pdfDoc); 
+		
+		return "Hello - Updated main controller";
+	}
 	
 }
