f9521688b06d1e0390c4483debc552e4294d2711
Bao Chau <chaub@oregonstate.edu>
2018-11-10 19:27:26 -0800

    14

---
+++ src/main/java/com/cs493/LatexResearch/CommandRunner.java
@@ -0,0 +1,70 @@
+package com.cs493.LatexResearch;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import com.cs493.LatexResearch.LatexResearchApplication;
+
+@Component
+public class CommandRunner implements CommandLineRunner {
+	Logger logger = LoggerFactory.getLogger(LatexResearchApplication.class);
+	
+	public static final Resource LATEX_DIR = new ClassPathResource("/latex_files");
+	public static final Resource LATEX_FILE = new ClassPathResource("/latex_files/document.tex");
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("\n>>> Start Latex_Research from CommandRunner...................");
+		
+		logger.info("\n>>> Call LATEX_DIR.getFile().getName() - 6...................");
+		String latexFolder = LATEX_DIR.getURL().toString();
+		String pdfDoc = latexFolder + "\\hello.pdf";
+		//String latexFile = latexFolder + "/document.tex";
+		
+		System.out.println(">>> latexFolder = " + latexFolder);
+		System.out.println(">>> pdfDoc = " + pdfDoc);
+		
+
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource("latex_files/document_2.tex").getFile());
+		System.out.println(">>> file getParent = " + file.getParent());
+		System.out.println(">>> file getPath = " + file.getPath());
+		System.out.println(">>> file getAbsolutePath = " + file.getAbsolutePath());
+		System.out.println(">>> file name = " + file.getName());
+		
+		
+		System.out.println("Run process - 1");
+		Process p = Runtime.getRuntime().exec("pdflatex " + file.getPath());
+		System.out.println("Run process - 2");		
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));		
+		BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		System.out.println("Run process - 3");
+		System.out.println("****************************************************************");
+        System.out.println("Result status:");		
+		String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+        
+        System.out.println("Result error:");
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
+        System.out.println("****************************************************************");
+        
+        
+		logger.info("\n>>> End Latex_Research from CommandRunner...................");
+
+	}
+	
+}
