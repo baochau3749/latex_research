5c1f6fc9e53a313abdf3a981ef99e67b298d6a53
Bao Chau <chaub@oregonstate.edu>
2018-11-10 18:56:16 -0800

    Trying to compile latex file using pdflatex.

--- src/main/java/com/cs493/LatexResearch/CommandRunner.java
+++ src/main/java/com/cs493/LatexResearch/CommandRunner.java
@@ -1,6 +1,9 @@
 package com.cs493.LatexResearch;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -26,6 +29,34 @@ public class CommandRunner implements CommandLineRunner {
 		System.out.println(">>> pdfDoc = " + pdfDoc);
 		
 		
+		//Process p = Runtime.getRuntime().exec("pdflatex C:\\heroku\\bc-test-buildpack\\test_latex_file.tex");
+		//Process p = Runtime.getRuntime().exec("pdflatex C:\\heroku\\bc-test-buildpack\\document.tex");
+		
+		System.out.println("Run process - 1");
+		//Process p = Runtime.getRuntime().exec("ipconfig");
+		//Process p = Runtime.getRuntime().exec("pdflatex C:\\heroku\\bc-test-buildpack\\document.tex");
+		Process p = Runtime.getRuntime().exec("pdflatex test_latex_file.tex");
+		System.out.println("Run process - 2");
+		//p.wait();
+		System.out.println("Run process - 3");
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		System.out.println("Run process - 4");
+		BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		
+		System.out.println("****************************************************************");
+        System.out.println("Result status:");		
+		String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+        
+        System.out.println("Result error:");
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
+        System.out.println("****************************************************************");
+        
+        
 		logger.info("\n>>> End Latex_Research from CommandRunner...................");
 
 	}
