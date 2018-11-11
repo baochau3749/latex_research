45eba1dad89f58fc8e14a2a641506bb4b3d9c237
Bao Chau <chaub@oregonstate.edu>
2018-11-10 19:32:48 -0800

    16

--- src/main/java/com/cs493/LatexResearch/CommandRunner.java
+++ src/main/java/com/cs493/LatexResearch/CommandRunner.java
@@ -18,25 +18,25 @@ import com.cs493.LatexResearch.LatexResearchApplication;
 public class CommandRunner implements CommandLineRunner {
 	Logger logger = LoggerFactory.getLogger(LatexResearchApplication.class);
 	
-	public static final Resource LATEX_DIR = new ClassPathResource("/latex_files");
-	public static final Resource LATEX_FILE = new ClassPathResource("/document.tex");
+	//public static final Resource LATEX_DIR = new ClassPathResource("/latex_files");
+	//public static final Resource LATEX_FILE = new ClassPathResource("/document.tex");
 	
 	
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("\n>>> Start Latex_Research from CommandRunner...................");
 		
-		logger.info("\n>>> Call LATEX_DIR.getFile().getName() - 6...................");
-		String latexFolder = LATEX_DIR.getURL().toString();
-		String pdfDoc = latexFolder + "\\hello.pdf";
-		//String latexFile = latexFolder + "/document.tex";
-		
-		System.out.println(">>> latexFolder = " + latexFolder);
-		System.out.println(">>> pdfDoc = " + pdfDoc);
+//		logger.info("\n>>> Call LATEX_DIR.getFile().getName() - 6...................");
+//		String latexFolder = LATEX_DIR.getURL().toString();
+//		String pdfDoc = latexFolder + "\\hello.pdf";
+//		//String latexFile = latexFolder + "/document.tex";
+//		
+//		System.out.println(">>> latexFolder = " + latexFolder);
+//		System.out.println(">>> pdfDoc = " + pdfDoc);
 		
 
 		ClassLoader classLoader = getClass().getClassLoader();
-		File file = new File(classLoader.getResource("latex_files/document_2.tex").getFile());
+		File file = new File(classLoader.getResource("document_2.tex").getFile());
 		System.out.println(">>> file getParent = " + file.getParent());
 		System.out.println(">>> file getPath = " + file.getPath());
 		System.out.println(">>> file getAbsolutePath = " + file.getAbsolutePath());
@@ -45,9 +45,11 @@ public class CommandRunner implements CommandLineRunner {
 		
 		System.out.println("Run process - 1");
 		Process p = Runtime.getRuntime().exec("pdflatex " + file.getPath());
+		
 		System.out.println("Run process - 2");		
 		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));		
 		BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		
 		System.out.println("Run process - 3");
 		System.out.println("****************************************************************");
         System.out.println("Result status:");		
