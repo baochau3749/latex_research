46d58af54bb16d59afee9f40a054fb2d6d426ed5
Bao Chau <chaub@oregonstate.edu>
2018-11-11 00:48:07 -0800

    44

--- src/main/java/com/cs493/LatexResearch/CommandRunner.java
+++ src/main/java/com/cs493/LatexResearch/CommandRunner.java
@@ -66,14 +66,19 @@ public class CommandRunner implements CommandLineRunner {
 //        cmd[3] = "target/classes/static/document_2.tex" ;
 
 		String[] cmd = new String[1];
-		cmd[0] = "pdfScript" ;
+		cmd[0] = "chmod +x ./target/classes/pdfScript" ;
 //        cmd[1] = "--output-format=pdf";
 //        cmd[2] = "--output-directory=target/classes/static";
 //        cmd[3] = "target/classes/static/document_2.tex" ;
 		
-		System.out.println("Run process - 3");
+		System.out.println("Run process - 3a");
+		Process p1 = Runtime.getRuntime().exec("chmod +x ./target/classes/pdfScript");
+		p1.waitFor();
+		
+		cmd[0] = "./target/classes/pdfScript" ;
+		System.out.println("Run process - 3b");
 		Process p = Runtime.getRuntime().exec(cmd);
-
+		
 		System.out.println("Run process - 4");		
 		BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));		
 		BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
