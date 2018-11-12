eeb73eae9dcf99b8f1d23c59d6d62ff7c2d7b235
Bao Chau <chaub@oregonstate.edu>
2018-11-11 18:22:05 -0800

    83

--- src/main/java/com/cs493/LatexResearch/Controller/MainController.java
+++ src/main/java/com/cs493/LatexResearch/Controller/MainController.java
@@ -74,15 +74,14 @@ public class MainController {
 
 		
 		String content;
-		String property = System.getProperty("user.dir") +"/src/main/resources/";
+		String property = System.getProperty("user.dir") +"/src/main/resources/static";
 		
 		//String filePath = "/" + MainController.class.getProtectionDomain().getCodeSource().getLocation().getPath()  + "application.properties";
-		String filePath = property + "application.properties";
+		String filePath = property + "my_text.txt";
 		content = filePath + "<br>";
 		try {
 
 			File file = ResourceUtils.getFile(filePath);
-            //InputStream in = new FileInputStream(file);
             content += new String(Files.readAllBytes(file.toPath()));
         } catch (IOException e) {
         	content += "error in reading data.";
